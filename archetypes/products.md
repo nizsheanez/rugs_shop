9df20b275ad42ab1e6b1201a3a40bedce910449d
alex.sharov <www.pismeco@gmail.com>
2018-06-06 14:22:15 +0700

    init

---
+++ archetypes/products.md
@@ -0,0 +1,14 @@
+{
+    "title": "{{ replace .Name "-" " " | title }}",
+    "date": "{{ .Date }}",
+    "tags": [],
+    "categories": [],
+    "images": [],
+    "thumbnailImage": "",
+    "actualPrice": "",
+    "comparePrice": "",
+    "inStock": true,
+    "variants": []
+}
+
+**Insert Product description here****
---
+++ themes/shopping-product-catalogue-simple/static/js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+    initializeLazyLoadOfImages();
+    $("input[type='radio']").change(function() {
+        selectPriceBasedOnVariant();
+    });
+    if (!_(productVariants).isEmpty()) {
+        selectPriceBasedOnVariant();
+    }
+})
+
+function initializeLazyLoadOfImages() {
+    var bLazy = new Blazy();
+}
+
+function getSelectedVariant(productVariants, selectedProductOption) {
+    return _(productVariants).filter(function(productVariant) {
+        return _(productVariant.optionCombination).isEqual(selectedProductOption)
+    }).first()
+}
+
+function selectPriceBasedOnVariant() {
+    var selectedProductOption = $("input[type='radio']:checked").val();
+    var selectedVariant = getSelectedVariant(productVariants, selectedProductOption);
+    var notInStock = !productInStock && (productActualPrice !== null);
+    if (selectedVariant) {
+        $(".product-actual-price").text(selectedVariant.actualPrice);
+        $(".product-compare-price").text(selectedVariant.comparePrice);
+        notInStock = !selectedVariant.inStock && (selectedVariant.actualPrice !== null);
+    }
+    $(".product-not-in-stock").toggle(notInStock);
+}
+
